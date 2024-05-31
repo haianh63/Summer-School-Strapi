1a1f379743dbaae069f8b03f217e4c3f7b4b0a55
Hai Anh <haianh6311@gmail.com>
2024-05-31 23:07:21 +0700

    Update content

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,66 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface BlogArticleHeadline extends Schema.Component {
+  collectionName: 'components_block_article_headlines';
+  info: {
+    displayName: 'headline';
+    description: '';
+  };
+  attributes: {
+    headline: Attribute.String & Attribute.Required;
+    slug: Attribute.String & Attribute.Required;
+  };
+}
+
+export interface BlogArticleLandscapeImage extends Schema.Component {
+  collectionName: 'components_block_article_landscape_images';
+  info: {
+    displayName: 'landscapeImage';
+    description: '';
+  };
+  attributes: {
+    image: Attribute.Media & Attribute.Required;
+    imageCaption: Attribute.String;
+  };
+}
+
+export interface BlogArticleParagraphWithImage extends Schema.Component {
+  collectionName: 'components_block_article_paragraph_with_images';
+  info: {
+    displayName: 'paragraphWithImage';
+    description: '';
+  };
+  attributes: {
+    paragraph: Attribute.RichText & Attribute.Required;
+    image: Attribute.Media & Attribute.Required;
+    isLandscape: Attribute.Boolean &
+      Attribute.Required &
+      Attribute.DefaultTo<true>;
+    imageShowsRight: Attribute.Boolean &
+      Attribute.Required &
+      Attribute.DefaultTo<true>;
+    imageCaption: Attribute.String;
+  };
+}
+
+export interface BlogArticleParagraph extends Schema.Component {
+  collectionName: 'components_block_article_paragraphs';
+  info: {
+    displayName: 'paragraph';
+    description: '';
+  };
+  attributes: {
+    paragraph: Attribute.RichText & Attribute.Required;
+  };
+}
+
 declare module '@strapi/strapi' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'blog-article.headline': BlogArticleHeadline;
+      'blog-article.landscape-image': BlogArticleLandscapeImage;
+      'blog-article.paragraph-with-image': BlogArticleParagraphWithImage;
+      'blog-article.paragraph': BlogArticleParagraph;
+    }
+  }
 }
